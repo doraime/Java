adb8d5639e4a6bdfd4168d3a664f90394dc2ff40
doraime <102696087+doraime@users.noreply.github.com>
2026-04-08 16:27:36 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,84 +1,21 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
-public class Main {
-    public static void main(String[] args) {
-      System.out.println("Unsorted Array ---------------------------------------------------");
-      ArrayList<Integer> integerList = Lab4.getList();
-      Lab4.outputList(integerList);
-
-      System.out.println("\n\nBubble sort results ----------------------------------------------");
-      long startTimeBubble = System.nanoTime();
-      ArrayList<Integer> bubbleSortedList = Lab4.bubbleSort(integerList);
-      long endTimeBubble = System.nanoTime();
-      long durationBubble = endTimeBubble - startTimeBubble;
-      System.out.println("Bubble Sort Time: " + durationBubble + " nanoseconds");
-      Lab4.outputList(bubbleSortedList);
-
-      System.out.println("\n\nInsertion sort results -------------------------------------------");
-      long startTimeInsertion = System.nanoTime();
-      ArrayList<Integer> insertionSortedList = Lab4.insertionSort(integerList);
-      long endTimeInsertion = System.nanoTime();
-      long durationInsertion = endTimeInsertion - startTimeInsertion;
-      System.out.println("Insertion Sort Time: " + durationInsertion + " nanoseconds");
-      Lab4.outputList(insertionSortedList);
-    }
-}
-
-class Lab4 {
-  public static ArrayList<Integer> insertionSort(ArrayList<Integer> integerList) {
-    // Step 1 - Implement insertion sort algorithm here
-
-    for (int i = 1; i < integerList.size(); i++) {
-        int key = integerList.get(i);
-        int j = i - 1;
-
-        // Move elements of integerList[0..i-1], that are greater than key,
-        // to one position ahead of their current position
-        while (j >= 0 && integerList.get(j) > key) {
-            integerList.set(j + 1, integerList.get(j));
-            j = j - 1;
-        }
-        integerList.set(j + 1, key);
-    }
-    return integerList;
-  }
-
-  public static ArrayList<Integer> bubbleSort(ArrayList<Integer> integerList) {
-    // Step 2 - Implement the bubble sort algorithm here
-
-    int n = integerList.size();
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (integerList.get(j) > integerList.get(j + 1)) {
-                // Swap elements
-                int temp = integerList.get(j);
-                integerList.set(j, integerList.get(j + 1));
-                integerList.set(j + 1, temp);
-            }
-        }
-    }
-    return integerList;
-  }
-
-  public static ArrayList<Integer> getList() {
-    ArrayList<Integer> integerList = new ArrayList<>();
-    String line;
-    try (BufferedReader br = new BufferedReader(new FileReader("integers.txt"))) {
-        while ((line = br.readLine()) != null) {
-            integerList.add(Integer.parseInt(line));
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    return integerList;
-  }
-
-  public static void outputList(ArrayList<Integer> integerList) {
-    for (int i = 0; i < integerList.size(); i++) {
-        System.out.print(integerList.get(i) + " ");
-    }
+class Main {
+  public static void main(String[] args) {
+    // Use the instructions in Blackboard or instructions.md to complete Lab 6
+    MyHashMap creditHours = new MyHashMap();
+    creditHours.put("IT-1025", 3);
+    creditHours.put("IT-1050",3);
+    creditHours.put("IT-1150",3);
+    creditHours.put("IT-2310",3);
+    creditHours.put("IT-2320",4);
+    creditHours.put("IT-2351", 4);
+    creditHours.put("IT-2650",4);
+    creditHours.put("IT-2660",4);
+    creditHours.put("IT-2030",4);
+    System.out.println(creditHours.containsKey("IT-1025"));
+    System.out.println(creditHours.containsKey("IT-2110"));
+    System.out.println(creditHours.toString());
+    creditHours.remove("IT-2030");
+    creditHours.remove("IT-1150");
+    System.out.println(creditHours.values());
   }
 }
--- challenge1.java
+++ challenge1.java
@@ -1,30 +1,22 @@
+package Lab6;
+import java.util.*;
 
 public class challenge1 {
-    public static void main(String[] args){
-        BST <Integer> tree = new BST<>();
-        tree.insert(50);
-        tree.insert(30);
-        tree.insert(70);
-        tree.insert(20);
-        tree.insert(40);
-        tree.insert(60);
-        tree.insert(80);
-        System.out.print("Size of the tree: ");
-        System.out.println(tree.size());
-        System.out.println(tree.search(60));
-        System.out.println(tree.search(25));
-        System.out.print("inorder: ");
-        tree.inorder();
-        System.out.print("\n");
-        System.out.print("postorder: ");
-        tree.postorder();
-        System.out.print("\n");
-        System.out.print("preorder: ");
-        tree.preorder();
-        System.out.print("\n");
-        tree.path(60);
-        tree.insert(65);
-        System.out.print("Size of the tree after inserting 65: ");
-        System.out.println(tree.size());
-    }
+public static void main(String[] args){
+    Map<String,String>map = new HashMap<>();
+    map.put("Smith","216-555-1234");
+    map.put("Johnson","216-555-2345");
+    map.put("Williams","216-555-3456");
+    map.put("Brown","216-555-4567");
+    map.put("Davis","216-555-5678");
+    map.put("Smith","216-555-9999");
+    System.out.println("Entries in map: "+map);
+    System.out.println("The phone number for Williams is "+map.get("Williams"));
+    System.out.println("Is Brown in the map? "+map.containsKey("Brown"));
+    System.out.println("I 216-555-0000 in the map? "+map.containsValue("216-555-0000"));
+    map.remove("Taylor");
+    System.out.println("Entries in map: "+map);
+    map.clear();
+    System.out.println("Entries in map after clearing: "+map);
+}
 }
